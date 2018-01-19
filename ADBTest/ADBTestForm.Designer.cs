c50113a2e39d626c09c5a9a1ef09d034cf5f767b
Leon <xiaoyankeji@hotmail.com>
2018-01-19 16:40:10 +0800

    first

---
+++ ADBTest/ADBTestForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ADBTest
+{
+    partial class ADBTestForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TaskButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TaskButton
+            // 
+            this.TaskButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TaskButton.Location = new System.Drawing.Point(0, 0);
+            this.TaskButton.Name = "TaskButton";
+            this.TaskButton.Size = new System.Drawing.Size(120, 44);
+            this.TaskButton.TabIndex = 0;
+            this.TaskButton.Text = "开始";
+            this.TaskButton.UseVisualStyleBackColor = true;
+            this.TaskButton.Click += new System.EventHandler(this.TaskButton_Click);
+            // 
+            // ADBTestForm
+            // 
+            this.AcceptButton = this.TaskButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(120, 44);
+            this.Controls.Add(this.TaskButton);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "ADBTestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ADBTest";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TaskButton;
+    }
+}
+
---
+++ ADBTest/ADBTestForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ADBTest
+{
+    public partial class ADBTestForm : Form
+    {
+        Hotkey UnityHotKey;
+        int SwitchHotKey;
+
+        Thread TestThread = null;
+        Process ADBProcess = new Process() { StartInfo = new ProcessStartInfo(@"D:\Desktops\adb\adb.exe") { WindowStyle = ProcessWindowStyle.Hidden } };
+
+        public ADBTestForm()
+        {
+            InitializeComponent();
+
+            //热键为 ALT+Z
+            UnityHotKey = new Hotkey(this.Handle);
+            SwitchHotKey = UnityHotKey.RegisterHotkey(Keys.Z, Hotkey.KeyFlags.MOD_ALT);
+            UnityHotKey.OnHotkey += new HotkeyEventHandler(OnHotkey);
+        }
+
+        public void OnHotkey(int HotkeyID)
+        {
+            if (HotkeyID == SwitchHotKey)
+            {
+                Switch();
+            }
+        }
+
+        private void Egg()
+        {
+            while (true)
+            {
+                ADBProcess.StartInfo.Arguments = "shell input swipe 360 1200 360 1200 20000";
+                ADBProcess.Start();
+                ADBProcess.WaitForExit();
+                Thread.Sleep(25000);
+            }
+        }
+
+        private void Switch()
+        {
+            if (TestThread == null)
+            {
+                TestThread = new Thread(Egg) { IsBackground = true };
+
+                TestThread.Start();
+            }
+            else
+            {
+                TestThread?.Abort();
+                TestThread = null;
+            }
+            TaskButton.Text = TestThread == null ? "已停止" : "已开始";
+            TaskButton.BackColor = TestThread == null ? Color.DeepSkyBlue : Color.OrangeRed;
+        }
+
+        private void TaskButton_Click(object sender, EventArgs e)
+        {
+            Switch();
+        }
+    }
+}
