343bf78952b33f7b584367653f0b87d75ae2930e
Leon <xiaoyankeji@hotmail.com>
2018-01-31 13:46:23 +0800

    完成：分离截图和自动领取奖励控件、自动检测奖励图标、判断图标种类、自动领取箱子；

--- ADBTest/ADBTestForm.Designer.cs
+++ ADBTest/ADBTestForm.Designer.cs
@@ -31,7 +31,8 @@
             this.TaskButton = new System.Windows.Forms.Button();
             this.SleepTimeout = new System.Windows.Forms.NumericUpDown();
             this.InputTimeout = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
+            this.ScreenShotCheckBox = new System.Windows.Forms.CheckBox();
+            this.OpenBoxCheckBox = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.SleepTimeout)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.InputTimeout)).BeginInit();
             this.SuspendLayout();
@@ -39,6 +40,11 @@
             // TaskButton
             // 
             this.TaskButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TaskButton.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.TaskButton.FlatAppearance.BorderSize = 5;
+            this.TaskButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.TaskButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.TaskButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.TaskButton.Location = new System.Drawing.Point(0, 0);
             this.TaskButton.Name = "TaskButton";
             this.TaskButton.Size = new System.Drawing.Size(163, 47);
@@ -51,14 +57,14 @@
             // 
             this.SleepTimeout.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.SleepTimeout.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.SleepTimeout.Location = new System.Drawing.Point(102, 47);
+            this.SleepTimeout.Location = new System.Drawing.Point(81, 47);
             this.SleepTimeout.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.SleepTimeout.Name = "SleepTimeout";
-            this.SleepTimeout.Size = new System.Drawing.Size(60, 26);
+            this.SleepTimeout.Size = new System.Drawing.Size(81, 26);
             this.SleepTimeout.TabIndex = 1;
             this.SleepTimeout.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.SleepTimeout.Value = new decimal(new int[] {
@@ -78,7 +84,7 @@
             0,
             0});
             this.InputTimeout.Name = "InputTimeout";
-            this.InputTimeout.Size = new System.Drawing.Size(60, 26);
+            this.InputTimeout.Size = new System.Drawing.Size(81, 26);
             this.InputTimeout.TabIndex = 2;
             this.InputTimeout.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.InputTimeout.Value = new decimal(new int[] {
@@ -87,23 +93,39 @@
             0,
             0});
             // 
-            // button1
+            // ScreenShotCheckBox
             // 
-            this.button1.Location = new System.Drawing.Point(61, 47);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(40, 26);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "截图";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ScreenShotCheckBox.BackColor = System.Drawing.Color.White;
+            this.ScreenShotCheckBox.Location = new System.Drawing.Point(1, 74);
+            this.ScreenShotCheckBox.Name = "ScreenShotCheckBox";
+            this.ScreenShotCheckBox.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.ScreenShotCheckBox.Size = new System.Drawing.Size(161, 21);
+            this.ScreenShotCheckBox.TabIndex = 4;
+            this.ScreenShotCheckBox.Text = "每刻自动截屏";
+            this.ScreenShotCheckBox.UseVisualStyleBackColor = false;
+            this.ScreenShotCheckBox.CheckedChanged += new System.EventHandler(this.ScreenShotCheckBox_CheckedChanged);
+            // 
+            // OpenBoxCheckBox
+            // 
+            this.OpenBoxCheckBox.BackColor = System.Drawing.Color.White;
+            this.OpenBoxCheckBox.Location = new System.Drawing.Point(1, 96);
+            this.OpenBoxCheckBox.Name = "OpenBoxCheckBox";
+            this.OpenBoxCheckBox.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.OpenBoxCheckBox.Size = new System.Drawing.Size(161, 21);
+            this.OpenBoxCheckBox.TabIndex = 5;
+            this.OpenBoxCheckBox.Text = "自动拾取箱子";
+            this.OpenBoxCheckBox.UseVisualStyleBackColor = false;
+            this.OpenBoxCheckBox.CheckedChanged += new System.EventHandler(this.OpenBoxCheckBox_CheckedChanged);
             // 
             // ADBTestForm
             // 
             this.AcceptButton = this.TaskButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(163, 74);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(163, 119);
+            this.Controls.Add(this.OpenBoxCheckBox);
+            this.Controls.Add(this.ScreenShotCheckBox);
             this.Controls.Add(this.InputTimeout);
             this.Controls.Add(this.SleepTimeout);
             this.Controls.Add(this.TaskButton);
@@ -126,7 +148,8 @@
         private System.Windows.Forms.Button TaskButton;
         private System.Windows.Forms.NumericUpDown SleepTimeout;
         private System.Windows.Forms.NumericUpDown InputTimeout;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox ScreenShotCheckBox;
+        private System.Windows.Forms.CheckBox OpenBoxCheckBox;
     }
 }
 
--- ADBTest/ADBTestForm.cs
+++ ADBTest/ADBTestForm.cs
@@ -14,15 +14,31 @@ namespace ADBTest
 {
     public partial class ADBTestForm : Form
     {
+        //TODO:使用UnityNotifyIcon弹出错误信息气泡提示
+
         Hotkey UnityHotKey;
         int SwitchHotKey;
 
-        Thread TestThread = null;
+        Thread ADBThread = null;
         Process ADBProcess = new Process() { StartInfo = new ProcessStartInfo(Path.Combine(Application.StartupPath, @"adb\adb.exe")) { WindowStyle = ProcessWindowStyle.Hidden } };
+
+        Thread ScreenShotThread = null;
+        Process ScreenShotProcess = new Process() { StartInfo = new ProcessStartInfo(Path.Combine(Application.StartupPath, @"adb\adb.exe")) { WindowStyle = ProcessWindowStyle.Hidden } };
+
+        Thread OpenBoxThread = null;
+        Process OpenBoxProcess = new Process() { StartInfo = new ProcessStartInfo(Path.Combine(Application.StartupPath, @"adb\adb.exe")) { WindowStyle = ProcessWindowStyle.Hidden } };
 
         NotifyIcon UnityNotifyIcon = new NotifyIcon();
         Bitmap StartIcon = new Bitmap(48, 48);
         Bitmap AbortIcon = new Bitmap(48, 48);
+
+        private enum IconType
+        {
+            Unknown,
+            News,
+            AD,
+            Box
+        }
 
         public ADBTestForm()
         {
@@ -34,7 +50,7 @@ namespace ADBTest
             UnityHotKey.OnHotkey += new HotkeyEventHandler(OnHotkey);
 
             using (Graphics IconGraphics = Graphics.FromImage(StartIcon))
-                IconGraphics.FillEllipse(Brushes.OrangeRed, 0, 0, 48, 48);
+                IconGraphics.FillEllipse(Brushes.Orange, 0, 0, 48, 48);
             using (Graphics IconGraphics = Graphics.FromImage(AbortIcon))
                 IconGraphics.FillEllipse(Brushes.DeepSkyBlue, 0, 0, 48, 48);
 
@@ -59,53 +75,249 @@ namespace ADBTest
                 ADBProcess.StartInfo.Arguments = string.Format("shell input swipe 360 1200 360 1200 {0}000", InputTimeout.Value);
                 ADBProcess.Start();
                 ADBProcess.WaitForExit();
-                if (DateTime.Now.Minute % 15 ==0) SaveScreen();//截图
-                Thread.Sleep(Convert.ToInt32(SleepTimeout.Value) * 1000);
+
+                ADBIdle();
             }
         }
 
-        private void SaveScreen()
+        /// <summary>
+        /// ADB进入空闲状态
+        /// </summary>
+        private void ADBIdle()
         {
-            ADBProcess.StartInfo.Arguments = string.Format("shell /system/bin/screencap -p /sdcard/screenshot-{0}.png", DateTime.Now.ToString("MM-dd+HH-mm-ss"));
-            ADBProcess.Start();
-            //ADBProcess.WaitForExit();
+            //截图
+            if (ScreenShotCheckBox.Checked && DateTime.Now.Minute % 15 == 0)
+            {
+                ScreenShot();
+            }
+            //打开箱子
+            if (OpenBoxCheckBox.Checked && DateTime.Now.Minute % 6 == 0)
+            {
+                
+            }
+
+            Thread.Sleep(Convert.ToInt32(SleepTimeout.Value) * 1000);
         }
 
+        /// <summary>
+        /// 异步截屏
+        /// </summary>
+        private void ScreenShot()
+        {
+            if (ScreenShotThread == null || ScreenShotThread.ThreadState == System.Threading.ThreadState.Stopped)
+            {
+                ScreenShotThread = new Thread(new ThreadStart(delegate
+                {
+                    ScreenShotProcess.StartInfo.Arguments = string.Format("shell /system/bin/screencap -p /sdcard/screenshot-{0}.png", DateTime.Now.ToString("MM-dd+HH-mm-ss"));
+                    ScreenShotProcess.Start();
+                    ScreenShotProcess.WaitForExit();
+                }))
+                { IsBackground = true };
+                ScreenShotThread.Start();
+            }
+        }
+
+        /// <summary>
+        /// 异步打开箱子
+        /// </summary>
+        private void OpenBox()
+        {
+            if (OpenBoxThread == null || OpenBoxThread.ThreadState == System.Threading.ThreadState.Stopped)
+            {
+                OpenBoxThread = new Thread(new ThreadStart(delegate 
+                {
+                    string ScreenShotPath = Path.Combine(Path.GetTempPath(), "screenshot-forbox.png");
+
+                    //截屏
+                    OpenBoxProcess.StartInfo.Arguments = "shell /system/bin/screencap -p /sdcard/screenshot-forbox.png";
+                    OpenBoxProcess.Start();
+                    OpenBoxProcess.WaitForExit();
+                    //获取截图
+                    OpenBoxProcess.StartInfo.Arguments = string.Format("pull /sdcard/screenshot-forbox.png {0}", ScreenShotPath);
+                    OpenBoxProcess.Start();
+                    OpenBoxProcess.WaitForExit();
+                    //File.Copy(ScreenShotPath, @"D:\1.png", true);
+                    //遍历箱子坐标
+                    foreach (Tuple<Point, bool> CheckResult in CheckBoxes(ScreenShotPath))
+                    {
+                        Debug.Print(CheckResult.Item1.ToString());
+                    }
+                }))
+                { IsBackground = true };
+                OpenBoxThread.Start();
+            }
+        }
+
+        /// <summary>
+        /// 检查箱子位置
+        /// </summary>
+        /// <param name="ScreenBoxPath">棘突文件路径</param>
+        /// <returns>箱子坐标，是否为广告</returns>
+        private IEnumerable<Tuple<Point, bool>> CheckBoxes(string ScreenBoxPath)
+        {
+            //报纸 x1
+            //广告 x1
+            //箱子 x2
+
+            if (!File.Exists(ScreenBoxPath)) yield break;
+
+            //!注意，这里需要从下向上寻找图标，并从下向上 yield return ，否则点击了上方的图标之后，下方的图标会移动到上方被点击后的图标位置
+            Point[] BoxLocations = new Point[] {
+                new Point(630, 430),
+                new Point(630, 340),
+                new Point(630, 250),
+                new Point(630, 160)
+            };
+            Bitmap ScreenShotBitmap = null;
+            using (FileStream ScreenShotStream = new FileStream(ScreenBoxPath, FileMode.Open))
+            {
+                ScreenShotBitmap = Image.FromStream(ScreenShotStream) as Bitmap;
+            }
+
+            //检查箱子位置
+            foreach (Point BoxLocation in BoxLocations)
+            {
+                if (IsWhite(ScreenShotBitmap.GetPixel(BoxLocation.X + 32, BoxLocation.Y + 8)) &&
+                    IsWhite(ScreenShotBitmap.GetPixel(BoxLocation.X + 32, BoxLocation.Y + 56)) &&
+                    IsWhite(ScreenShotBitmap.GetPixel(BoxLocation.X + 8, BoxLocation.Y + 32)) &&
+                    IsWhite(ScreenShotBitmap.GetPixel(BoxLocation.X + 56, BoxLocation.Y + 32))
+                    )
+                {
+                    switch (GetIconType(ScreenShotBitmap.GetPixel(BoxLocation.X + 32, BoxLocation.Y + 32)))
+                    {
+                        case IconType.Box:
+                            {
+                                //Button: {210, 766, 300, 80}
+                                Debug.Print("盒子");
+                                OpenBoxProcess.StartInfo.Arguments = string.Format("shell input tap {0} {1}", BoxLocation.X + 32, BoxLocation.Y + 32);
+                                OpenBoxProcess.Start();
+                                OpenBoxProcess.WaitForExit();
+                                OpenBoxProcess.StartInfo.Arguments = string.Format("shell input tap {0} {1}", 210, 766);
+                                OpenBoxProcess.Start();
+                                OpenBoxProcess.WaitForExit();
+                                break;
+                            }
+                        case IconType.AD:
+                            {
+                                Debug.Print("广告");
+                                break;
+                            }
+                        case IconType.News:
+                            {
+                                Debug.Print("新闻");
+                                break;
+                            }
+                        case IconType.Unknown:
+                            {
+                                Debug.Print("未知的图标类型");
+                                break;
+                            }
+                        default:
+                            {
+                                Debug.Print("其他的图标类型");
+                                break;
+                            }
+                    }
+                    yield return new Tuple<Point, bool>(new Point(BoxLocation.X + 32, BoxLocation.Y + 32), true);
+                }
+            }
+
+            ScreenShotBitmap?.Dispose();
+        }
+
+        /// <summary>
+        /// 检查颜色是否为白色
+        /// </summary>
+        /// <returns>是否为白色</returns>
+        private bool IsWhite(Color TargetColor)
+        {
+            return (
+                TargetColor.R == 255 &&
+                TargetColor.G == 255 &&
+                TargetColor.B == 255
+                );
+        }
+
+        /// <summary>
+        /// 根据图标中心颜色获取图标类型
+        /// </summary>
+        /// <param name="TargetColor">图标中心颜色</param>
+        /// <returns></returns>
+        private IconType GetIconType(Color TargetColor)
+        {
+            Debug.Print(TargetColor.ToString());
+
+            //Color[A = 255, R = 159, G = 168, B = 151]
+            if (TargetColor.G > TargetColor.R && TargetColor.R > TargetColor.B)
+                return IconType.News;
+
+            //Color[A = 255, R = 255, G = 255, B = 255]
+            if (TargetColor.R == TargetColor.G && TargetColor.G == TargetColor.B)
+                return IconType.AD;
+            
+            //Color[A = 255, R = 165, G = 141, B = 70]
+            if (TargetColor.R > TargetColor.G && TargetColor.G > TargetColor.B)
+                return IconType.Box;
+
+            return IconType.Unknown;
+        }
+
+        /// <summary>
+        /// 切换开关
+        /// </summary>
         private void Switch()
         {
-            if (TestThread == null)
+            if (ADBThread == null)
             {
-                TestThread = new Thread(Egg) { IsBackground = true };
-                TestThread.Start();
+                ADBThread = new Thread(Egg) { IsBackground = true };
+                ADBThread.Start();
                 this.TopMost = true;
-
             }
             else
             {
-                if (!ADBProcess.HasExited) ADBProcess.Kill();
-                TestThread?.Abort();
-                TestThread = null;
+                try
+                {
+                    if (!ADBProcess.HasExited) ADBProcess.Kill();
+                    if (!ScreenShotProcess.HasExited) ScreenShotProcess.Kill();
+                    if (!OpenBoxProcess.HasExited) OpenBoxProcess.Kill();
+                }
+                catch{ }
+
+                ADBThread?.Abort();
+                ADBThread = null;
+                ScreenShotThread?.Abort();
+                ScreenShotThread = null;
+                OpenBoxThread?.Abort();
+                OpenBoxThread = null;
             }
-            TaskButton.Text = TestThread == null ? "已停止" : "已开始";
+
+            TaskButton.Text = ADBThread == null ? "已停止" : "已开始";
             UnityNotifyIcon.Text = TaskButton.Text;
-            UnityNotifyIcon.Icon = Icon.FromHandle((TestThread == null ? AbortIcon : StartIcon).GetHicon());
-            TaskButton.BackColor = TestThread == null ? Color.DeepSkyBlue : Color.OrangeRed;
+            UnityNotifyIcon.Icon = Icon.FromHandle((ADBThread == null ? AbortIcon : StartIcon).GetHicon());
+            TaskButton.BackColor = ADBThread == null ? Color.DeepSkyBlue : Color.Orange;
         }
 
         private void TaskButton_Click(object sender, EventArgs e)
         {
             Switch();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SaveScreen();
-        }
-
+        
         private void ADBTestForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             UnityHotKey.UnregisterHotkeys();
             UnityNotifyIcon.Dispose();
+        }
+
+        private void ScreenShotCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ScreenShotCheckBox.Checked)
+                ScreenShot();
+        }
+
+        private void OpenBoxCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (OpenBoxCheckBox.Checked)
+                OpenBox();
         }
 
     }
